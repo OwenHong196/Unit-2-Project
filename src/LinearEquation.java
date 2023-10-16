d00ff3cd1cba6f0a7ecd4e40a38e4fbe53ae12c9
unknown <BT_2N2_26@BKTECH.LOCAL>
2023-10-16 14:05:31 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,9 @@
+public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    public LinearEquation(int x1,int y1, int x2,int y2){
+
+    }
+}
---
+++ src/LinearEquationLogic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class LinearEquationLogic {
+    Scanner scan = new Scanner(System.in);
+    public void start(){ }
+
+    private void getData(){
+        System.out.println("Welcome user!");
+        System.out.println("Enter coordinate 1: ");
+        String coor1 = scan.nextLine();
+        int coor1X = parse(coor1.substring(1,coor1.indexOf(",")));
+        int coor1Y = parse(coor1.substring(coor1.indexOf(",")+1,coor1.length()-1));
+        System.out.println("Enter coordinate 2: ");
+        String coor2 = scan.nextLine();
+        int coor2X = parse(coor2.substring(1,coor2.indexOf(",")));
+        int coor2Y = parse(coor2.substring(coor2.indexOf(",")+1,coor2.length()-1));
+        LinearEquation equation = new LinearEquation(coor1X,coor1Y,coor2X,coor2Y);
+    }
+    public static int parse(String num){
+        return Integer.parseInt(num);
+    }
+}
